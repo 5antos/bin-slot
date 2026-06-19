f96cc2319e112ae42bea5f846a8983f1814635f1
Pedro Santos <pemiridosa@gmail.com>
2026-06-19 17:56:01 +0100

    update package and project ownership

---
+++ src/main/java/net/pemiridosa/binslot/network/payload/MouseClickC2SPayload.java
@@ -0,0 +1,28 @@
+package net.pemiridosa.binslot.network.payload;
+
+import net.pemiridosa.binslot.util.Constants;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
+
+public record MouseClickC2SPayload(ItemStack itemStack, boolean isRightClick, boolean isShiftClick, boolean isCreativeInventory) implements CustomPayload {
+    public static final CustomPayload.Id<MouseClickC2SPayload> ID = new CustomPayload.Id<>(Constants.MOUSE_CLICK_PAYLOAD_ID);
+    public static final PacketCodec<RegistryByteBuf, MouseClickC2SPayload> CODEC = PacketCodec.tuple(
+        ItemStack.OPTIONAL_PACKET_CODEC,
+        MouseClickC2SPayload::itemStack,
+        PacketCodecs.BOOLEAN,
+        MouseClickC2SPayload::isRightClick,
+        PacketCodecs.BOOLEAN,
+        MouseClickC2SPayload::isShiftClick,
+        PacketCodecs.BOOLEAN,
+        MouseClickC2SPayload::isCreativeInventory,
+        MouseClickC2SPayload::new
+    );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
